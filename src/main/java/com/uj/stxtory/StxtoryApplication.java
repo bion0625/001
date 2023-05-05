eaf537c5f4c5ca532a62596d1739324e09dc5e2b
이의정 <stxtory@gmail.com>
2023-05-05 09:13:57 +0900

    001 init

---
+++ src/main/java/com/uj/stxtory/StxtoryApplication.java
@@ -0,0 +1,13 @@
+package com.uj.stxtory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StxtoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StxtoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uj/stxtory/StxtoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uj.stxtory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StxtoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
