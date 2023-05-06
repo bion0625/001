b1d1ccad30ef99952b3c15eb1f70cf91f758cb38
onairsparks <onairsparks@gmail.com>
2023-05-07 08:07:35 +0900

    js 경로 설정 및 bootstrap 기본 설정

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,19 @@
+function login(){
+    fetch('/', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            userId: document.querySelector('#userId').value,
+            userPassword: document.querySelector('#userPassword').value,
+        }),
+    }).then(data => data.json())
+        .then(data => {
+            if(data.loginCheck === "SUCCESS"){
+                location.href="/main";
+            }else{
+                alert(data.loginCheck);
+            }
+        });
+}
