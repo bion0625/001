5db012ced6d4ae5733645e688d9371e931348437
bion0625 <bion0625@gmail.com>
2024-07-12 08:10:45 +0900

    DB 날짜 포맷 설정

--- src/main/java/com/uj/stxtory/domain/entity/Base.java
+++ src/main/java/com/uj/stxtory/domain/entity/Base.java
@@ -4,24 +4,19 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 public abstract class Base {
-    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-    private Date createdAt = new Date();
+    private LocalDateTime createdAt = LocalDateTime.now();
 
-    @Temporal(TemporalType.TIMESTAMP)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     @Column(name = "updated_at", nullable = true)
-    private Date updatedAt;
+    private LocalDateTime updatedAt;
 
-    @Temporal(TemporalType.TIMESTAMP)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     @Column(name = "deleted_at", nullable = true)
-    private Date deletedAt;
+    private LocalDateTime deletedAt;
 }
