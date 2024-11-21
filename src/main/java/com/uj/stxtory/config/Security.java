59fa2ea5523142f56ef503619598e387bbbdf516
stxtory <stxtory@gmail.com>
2024-11-21 09:28:39 +0900

    security: ant 표현식보다 안전한 mvc 표현식으로 변경

--- src/main/java/com/uj/stxtory/config/Security.java
+++ src/main/java/com/uj/stxtory/config/Security.java
@@ -22,8 +22,8 @@ public class Security implements WebMvcConfigurer {
         http
                 .csrf().disable()//todo loginPagecustom을 위해 추가 한 부분, 검색 필요
                 .authorizeRequests()
-//                .antMatchers("/", "/home", "/join").permitAll() // 설정한 리소스의 접근을 인증절차 없이 허용
-                .antMatchers("/join").permitAll() // 설정한 리소스의 접근을 인증절차 없이 허용
+//                .mvcMatchers("/", "/home", "/join").permitAll() // 설정한 리소스의 접근을 인증절차 없이 허용
+                .mvcMatchers("/join").permitAll() // 설정한 리소스의 접근을 인증절차 없이 허용
                 .anyRequest().authenticated() // 그 외 모든 리소스를 의미하며 인증 필요
                 .and()
                 .formLogin()
