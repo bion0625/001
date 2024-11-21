e87a8ce4e5e9536ae371b26287348a6fde7cce51
stxtory <stxtory@gmail.com>
2024-11-21 14:06:23 +0900

    권한 설정 추가

--- src/main/java/com/uj/stxtory/config/SecurityConfig.java
+++ src/main/java/com/uj/stxtory/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,17 +20,17 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable();
+        http.csrf(AbstractHttpConfigurer::disable);
 
         http
-                .formLogin()
-                .permitAll()
-                .loginPage("/login")
-                .defaultSuccessUrl("/", true);
+                .formLogin(form -> form.permitAll()
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true));
         http
-                .authorizeRequests()
-                .mvcMatchers("/join").permitAll()
-                .anyRequest().authenticated();
+                .authorizeRequests(request -> request
+                        .mvcMatchers("/join").permitAll()
+                        .mvcMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated());
 
         return http.build();
     }
---
+++ src/main/java/com/uj/stxtory/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.uj.stxtory.controller;
+
+import com.uj.stxtory.service.mail.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @Autowired
+    MailService mailService;
+
+    @GetMapping(value = "/admin")
+    public String admin(Model model){
+        model.addAttribute("targets", mailService.getTargets());
+        return "/main";
+    }
+
+    @GetMapping(value = "/admin/test")
+    public String adminTest(Model model){
+        model.addAttribute("targets", mailService.getTargets());
+        return "/main";
+    }
+}
