5ca3234c960f51c5cb21f9bfe9d7d531ef81e868
bion0625 <bion0625@gmail.com>
2024-11-27 14:32:00 +0900

    rest data 기능 추가 (sql_:url prefix)

--- src/main/java/com/uj/stxtory/repository/UPbitRepository.java
+++ src/main/java/com/uj/stxtory/repository/UPbitRepository.java
@@ -2,9 +2,11 @@ package com.uj.stxtory.repository;
 
 import com.uj.stxtory.domain.entity.UPbit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "sql_upbit")
 public interface UPbitRepository extends JpaRepository<UPbit, Long> {
     List<UPbit> findAllByDeletedAtIsNull();
 }
