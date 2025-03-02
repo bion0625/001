0cdc28634da97bbda3f1f1c09bc957294d92957b
이의정 <stxtory@gmail.com>
2025-03-02 14:43:49 +0900

    DealSettings 로그 적용

--- trade-service/src/main/java/com/uj/stxtory/service/deal/scheduler/StockSchedulerService.java
+++ trade-service/src/main/java/com/uj/stxtory/service/deal/scheduler/StockSchedulerService.java
@@ -1,12 +1,11 @@
 package com.uj.stxtory.service.deal.scheduler;
 
-import com.uj.stxtory.config.DealDaysConfig;
+import com.uj.stxtory.service.DealSettingsService;
 import com.uj.stxtory.service.deal.DealSchedulerService;
 import com.uj.stxtory.service.deal.notify.StockNotifyService;
 import com.uj.stxtory.service.mail.MailService;
 import com.uj.stxtory.util.ApiUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,24 +16,22 @@ import java.util.ArrayList;
 @Transactional
 @Service
 public class StockSchedulerService implements DealSchedulerService {
-    @Autowired
     private final MailService mailService;
-    @Autowired
     private final StockNotifyService stockNotifyService;
-    @Autowired
-    private final DealDaysConfig dealDaysConfig;
+    private final DealSettingsService dealSettingsService;
 
-    public StockSchedulerService(MailService mailService, StockNotifyService stockNotifyService, DealDaysConfig dealDaysConfig) {
+    public StockSchedulerService(MailService mailService, StockNotifyService stockNotifyService, DealSettingsService dealSettingsService) {
         this.mailService = mailService;
         this.stockNotifyService = stockNotifyService;
-        this.dealDaysConfig = dealDaysConfig;
+        this.dealSettingsService = dealSettingsService;
     }
 
     // 월-금 아침 8시 - 오후 4시: 정각 및 20분, 40분 마다
     @Override
     @Scheduled(cron = "0 0/15 8-16 ? * MON-FRI")
     public void save() {
-        int baseDays = dealDaysConfig.getStockBaseDays();
+        int baseDays = dealSettingsService.getByName("stock").getHighestPriceReferenceDays();
+        log.info("\n\n\nstock save start("+ baseDays +")\n\n\n");
         stockNotifyService.save();
         log.info("\n\n\nstock save complete("+ baseDays +")\n\n\n");
     }
@@ -43,6 +40,7 @@ public class StockSchedulerService implements DealSchedulerService {
     @Override
     @Scheduled(cron = "0 0/15 8-16 ? * MON-FRI")
     public void update() {
+        log.info("\n\n\nstock update & mail send start\n\n\n");
         ApiUtil.runWithException(
                 () -> mailService.noticeDelete(stockNotifyService.update().getDeleteItems(), "STOCK"));
         log.info("\n\n\nstock update & mail send complete\n\n\n");
