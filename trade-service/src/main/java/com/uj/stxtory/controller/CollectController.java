bda3b6c276925cb566d71d6780ea9b00b89c2ac2
bion0625 <stxtory@gmail.com>
2025-06-04 14:47:12 +0900

    로그 추가

--- trade-service/src/main/java/com/uj/stxtory/controller/CollectController.java
+++ trade-service/src/main/java/com/uj/stxtory/controller/CollectController.java
@@ -1,10 +1,12 @@
 package com.uj.stxtory.controller;
 
 import com.uj.stxtory.service.deal.notify.StockNotifyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class CollectController {
 
   private final StockNotifyService stockNotifyService;
@@ -15,7 +17,9 @@ public class CollectController {
 
   @GetMapping(value = "/collect/stock")
   public String stocksHistoryCollect() {
+    log.info("\n\n\nstock saveHistory start\n\n\n");
     stockNotifyService.saveHistory();
+    log.info("\n\n\nstock saveHistory complete\n\n\n");
     return "success";
   }
 }
