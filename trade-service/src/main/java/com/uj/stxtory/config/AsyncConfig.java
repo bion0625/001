6deafe0c66449c02f75527a2a46b2f6a7a167b55
bion0625 <bion0625@gmail.com>
2025-07-13 10:31:11 +0900

    비동기 설정 추가

--- trade-service/src/main/java/com/uj/stxtory/config/AsyncConfig.java
+++ trade-service/src/main/java/com/uj/stxtory/config/AsyncConfig.java
@@ -3,10 +3,12 @@ package com.uj.stxtory.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
   @Bean
   ThreadPoolTaskExecutor poolTaskExecutor() {
